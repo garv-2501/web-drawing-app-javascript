017dce3f1cda6968d75cdbbea1b4375a841b3a4a
garv-2501 <garvsinghrawlot1.0@gmail.com>
2022-08-14 00:42:12 +0530

    starting the scissor tool

---
+++ toolboxTools/scissorTool.js
@@ -0,0 +1,11 @@
+function ScissorTool() {
+  // set an icon and a name for the object
+  this.name = "Scissor Tool";
+  this.icon = "assets/scissorTool.png";
+
+  this.draw = function () {
+  }
+
+  this.populateOptions = function () {
+  }
+}
